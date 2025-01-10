baf96016da1855eff869c5bb4fa456217c1f9273
Diogo Realles <diogorealles@hotmail.com>
2025-01-10 09:40:37 -0300

    [FEATURE] Fim

--- tailwindcss/forest/src/script.js
+++ tailwindcss/forest/src/script.js
@@ -1,3 +1,20 @@
+const video = document.querySelector('#video');
+const temperature = document.querySelector('#temperature');
+const weather = document.querySelector('#weather');
+const week = document.querySelector('#week');
+
+const random = Math.floor(Math.random() * 10) + 20;
+temperature.innerText = `${random}°`;
+weather.innerText = random < 25 ? `🌧️ ${random + 5}%` : `🌤️`;
+const date = new Date();
+week.innerText = date
+  .toLocaleDateString('pt-BR', {
+    weekday: 'long',
+  })
+  .replace('-feira', '');
+
+video.src = random < 25 ? `./img/video_chuva.mp4` : `./img/video_sol.mp4`;
+
 const mobileButton = document.querySelector('#mobile-button');
 const mobileMenu = document.querySelector('#mobile-menu');
 
